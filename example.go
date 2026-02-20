beaf4db699a358294bfcc1772256e4ba338da812
Nicholas John Childs <njchilds90@gmail.com>
2026-02-20 10:42:15 -0500

    Add example.go to demonstrate dotenv usage
    This file demonstrates how to use the dotenv package to load environment variables from a .env file.

---
+++ example.go
@@ -0,0 +1,21 @@
+// example.go demonstrates usage of the dotenv package.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/njchilds90/go-dotenv-simple/dotenv"
+)
+
+func main() {
+	err := dotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env:", err)
+		return
+	}
+
+	value := os.Getenv("EXAMPLE_VAR")
+	fmt.Println("EXAMPLE_VAR:", value)
+}
